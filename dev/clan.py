fb3b22ae91afd6274c8061ddeae1431b600ba681
Михаил Драган <@itstep.krg>
2023-02-07 20:26:21 +0600

    a

---
+++ dev/clan.py
@@ -0,0 +1,15 @@
+class Client:
+    """Client for git hub"""
+
+def __init__(
+    self,
+    name:str,
+    login: str,
+    password:int
+) -> None:
+    self.name = name
+    self.login = login
+    self.password = password
+
+
+
